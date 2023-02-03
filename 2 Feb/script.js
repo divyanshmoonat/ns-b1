e89416f9e522bae66a3adef59df38256a6319784
Divyansh Moonat <divyanshmoonat@outlook.com>
2023-02-03 21:10:03 +0530

    Timer assignment

---
+++ 2 Feb/script.js
@@ -0,0 +1,14 @@
+// console.log("Stopwatch");
+/**
+ * Step 1: Find all element required using getElementById()
+ * Step 2: Register event listeners on all three buttons (click)
+ * Step 3: Take 3 variables hh, mm and ss and initialize them with 0, interval initilize with null
+ * Step 4: Start Btn : Register a set interval with 1000ms (1s) and store it in interval variable
+ * Step 5: Stop Btn : Clear the interval and reset the hh,mm and ss variable to their initial value
+ */
+
+
+let hh=0;
+let mm=0;
+let ss=0;
+let interval=null;
