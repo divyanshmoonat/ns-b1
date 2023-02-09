7939f756de0265821c0baa62e7de18abecf6e0b7
Divyansh Moonat <divyanshmoonat@outlook.com>
2023-02-09 23:04:23 +0530

    ReactJS introduction

---
+++ React-Introduction/calculator.js
@@ -0,0 +1,12 @@
+const add = (num1, num2) => {
+  console.log(num1 + num2);
+};
+
+const subtract = (num1, num2) => {
+  console.log(num1 - num2);
+};
+
+module.exports = {
+  add,
+  subtract,
+};
---
+++ React-Introduction/index.js
@@ -0,0 +1,8 @@
+const calculator = require("./calculator");
+
+console.log("This is my first NPM based React Project");
+let a = 10;
+let b = 50;
+
+calculator.add(a, b);
+calculator.subtract(a, b);
